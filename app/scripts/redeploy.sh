8a906d61a1fbf012da02855f327ac1213476541f
Chizy <32227554+chizy7@users.noreply.github.com>
2023-02-28 20:29:41 -0500

    Cleaning

--- app/scripts/redeploy.sh
+++ app/scripts/redeploy.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 
-# cd ~/repos/project-zara;
-
-cd /root/project-zara;
+cd /root/project-zara
 
 # Fetch the latest changes from the main branch and reset the local repository
-git fetch && git reset origin/main --hard;
-# git stash && git switch main && git fetch --all && git reset --hard origin/main;
+git fetch && git reset origin/main --hard
 
 # Activate the Python virtual environment and install dependensies
-# source venv/bin/activate;
-source python3-virtualenv/bin/activate;
+source python3-virtualenv/bin/activate
+pip install -r requirements.txt
 
-# pip install -q -r requirements.txt;
-pip install -r requirements.txt;
-
-
-systemctl daemon-reload;
-systemctl restart myportfolio.service;
+# Systemctl
+systemctl daemon-reload
+systemctl restart myportfolio.service
 
 echo "Complete."

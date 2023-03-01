7e2f56aebbe4ec951fc009f78d53efe64a81bb58
Chizy <32227554+chizy7@users.noreply.github.com>
2023-02-28 20:27:22 -0500

    Bug fixes

--- app/scripts/redeploy.sh
+++ app/scripts/redeploy.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-cd ~/repos/project-zara;
+# cd ~/repos/project-zara;
 
-# cd /root/project-zara
+cd /root/project-zara;
 
 # Fetch the latest changes from the main branch and reset the local repository
-# git fetch && git reset origin/main --hard;
-git stash && git switch main && git fetch --all && git reset --hard origin/main;
+git fetch && git reset origin/main --hard;
+# git stash && git switch main && git fetch --all && git reset --hard origin/main;
 
 # Activate the Python virtual environment and install dependensies
-source venv/bin/activate;
-# source python3-virtualenv/bin/activate
+# source venv/bin/activate;
+source python3-virtualenv/bin/activate;
 
-pip install -q -r requirements.txt;
-# pip install -r requirements.txt
+# pip install -q -r requirements.txt;
+pip install -r requirements.txt;
 
 
 systemctl daemon-reload;

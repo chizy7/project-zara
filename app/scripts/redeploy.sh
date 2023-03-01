38b27893474dda7ab98b7a0f4f00e155bdcb41b1
Chizy <32227554+chizy7@users.noreply.github.com>
2023-02-28 20:24:07 -0500

    fix

--- app/scripts/redeploy.sh
+++ app/scripts/redeploy.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-# Kill all existing tmux sessions
-tmux kill-server
-
 cd ~/repos/project-zara;
 
+# cd /root/project-zara
+
 # Fetch the latest changes from the main branch and reset the local repository
-git fetch && git reset origin/main --hard
+# git fetch && git reset origin/main --hard;
+git stash && git switch main && git fetch --all && git reset --hard origin/main;
 
 # Activate the Python virtual environment and install dependensies
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
+source venv/bin/activate;
+# source python3-virtualenv/bin/activate
+
+pip install -q -r requirements.txt;
+# pip install -r requirements.txt
+
 
 systemctl daemon-reload;
-systemctl restart project-zara.service;
+systemctl restart myportfolio.service;
 
 echo "Complete."

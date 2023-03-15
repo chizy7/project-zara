44157d9bdeb97ced6d095707233a79dc281812e6
Chizy <chizy7@gmail.com>
2023-03-15 09:50:50 -0400

    Script updated

--- app/scripts/redeploy-site.sh
+++ app/scripts/redeploy-site.sh
@@ -9,12 +9,10 @@ cd /root/project-zara
 # Fetch the latest changes from the main branch and reset the local repository
 git fetch && git reset origin/main --hard
 
-# Activate the Python virtual environment and install dependensies
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
+# Stop and remove any existing containers
+docker-compose -f docker-compose.prod.yml down
 
-# Start a new detached Tmux session in the project directory
-tmux new-session -d -s myproject 'cd /root/project-zara && source python3-virtualenv/bin/activate && export FLASK_ENV=production && flask run --host=0.0.0.0'
+# Build and start the new containers in detached mode
+docker-compose -f docker-compose.prod.yml up -d --build
 
-
-echo "Deployment complete."
+echo "Docker Build Complete."

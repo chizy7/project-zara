be88e2b70b26824a53867ce11093c6162ebd1a0f
Chizy <chizy7@gmail.com>
2023-03-25 20:27:44 -0400

    Tests todo fix

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,74 +1,74 @@
-# import unittest
-# from peewee import *
+import unittest
+from peewee import *
 
-# # Use an in-memory SQLite for tests.
-# test_db = SqliteDatabase(':memory:')
+# Use an in-memory SQLite for tests.
+test_db = SqliteDatabase(':memory:')
 
-# from app import app
-# from app import TimelinePost
-# from app import mydb
+from app import app
+from app import TimelinePost
+from app import mydb
 
-# mydb.initialize(test_db)
+mydb.initialize(test_db)
 
-# MODELS = [TimelinePost]
+MODELS = [TimelinePost]
 
-# class TestTimelinePost(unittest.TestCase):
-#     def setUp(self):
-#         # Bind model classes to test db. Since we have a complete list of all models,
-#         # we do not need to recursively bind dependencies. 
-#         test_db.bind(MODELS, bind_refs=False, bind_backrefs=False)
+class TestTimelinePost(unittest.TestCase):
+    def setUp(self):
+        # Bind model classes to test db. Since we have a complete list of all models,
+        # we do not need to recursively bind dependencies. 
+        test_db.bind(MODELS, bind_refs=False, bind_backrefs=False)
 
-#         test_db.connect()
-#         test_db.create_tables(MODELS)
+        test_db.connect()
+        test_db.create_tables(MODELS)
 
-#     def tearDown(self):
-#         # Not strictly necessary since SQLite in-memory databases only live for
-#         # the duration of the connection, and in the next step we close the 
-#         # connection...but a good practice all the same.
-#         test_db.drop_tables(MODELS)
+    def tearDown(self):
+        # Not strictly necessary since SQLite in-memory databases only live for
+        # the duration of the connection, and in the next step we close the 
+        # connection...but a good practice all the same.
+        test_db.drop_tables(MODELS)
 
-#         # Close connection to db.
-#         test_db.close()
+        # Close connection to db.
+        test_db.close()
 
-#     # ... rest of the code ...
+    # ... rest of the code ...
 
 
-#     def test_timeline_post(self):
-#         # Create 2 timeline posts.
-#         first_post = TimelinePost.create(name='John Doe', email='john@example.com', content='Hello world, I\'m John!')
-#         assert first_post.id == 1
-#         second_post = TimelinePost.create(name='Jane Doe', email='jane@example.com', content='Hello world, I\'m Jane!')
-#         assert second_post.id == 2
+    def test_timeline_post(self):
+        # Create 2 timeline posts.
+        first_post = TimelinePost.create(name='John Doe', email='john@example.com', content='Hello world, I\'m John!')
+        assert first_post.id == 1
+        second_post = TimelinePost.create(name='Jane Doe', email='jane@example.com', content='Hello world, I\'m Jane!')
+        assert second_post.id == 2
 
-#         # TODO: Get timeline posts and assert that they are correct
-#     def test_get_time_line_post(self):
-#         # Create 2 timeline posts.
-#         first_post = TimelinePost.create(name='John Doe', email='john@example.com', content='Hello world, I\'m John!')
-#         second_post = TimelinePost.create(name='Jane Doe', email='jane@example.com', content='Hello world, I\'m Jane!')
+        # TODO: Get timeline posts and assert that they are correct
+    def test_get_time_line_post(self):
+        # Create 2 timeline posts.
+        first_post = TimelinePost.create(name='John Doe', email='john@example.com', content='Hello world, I\'m John!')
+        second_post = TimelinePost.create(name='Jane Doe', email='jane@example.com', content='Hello world, I\'m Jane!')
 
-#         # Get timeline posts.
-#         response = app.test_client().get('/api/timeline_post')
-#         data = response.get_json()
+        # Get timeline posts.
+        response = app.test_client().get('/api/timeline_post')
+        data = response.get_json()
 
-#         # Assert that the response is correct.
-#         self.assertEqual(response.status_code, 200)
-#         self.assertIsInstance(data, dict)
-#         self.assertIn('timeline_posts', data)
-#         self.assertIsInstance(data['timeline_posts'], list)
-#         self.assertEqual(len(data['timeline_posts']), 2)
+        # Assert that the response is correct.
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(data, dict)
+        self.assertIn('timeline_posts', data)
+        self.assertIsInstance(data['timeline_posts'], list)
+        self.assertEqual(len(data['timeline_posts']), 2)
 
-#         # Assert the first post.
-#         first_post_data = data['timeline_posts'][0]
-#         self.assertIsInstance(first_post_data, dict)
-#         self.assertEqual(first_post_data['id'], first_post.id)
-#         self.assertEqual(first_post_data['name'], 'John Doe')
-#         self.assertEqual(first_post_data['email'], 'john@example.com')
-#         self.assertEqual(first_post_data['content'], "Hello world, I'm John!")
+        # Assert the first post.
+        first_post_data = data['timeline_posts'][0]
+        self.assertIsInstance(first_post_data, dict)
+        self.assertEqual(first_post_data['id'], first_post.id)
+        self.assertEqual(first_post_data['name'], 'John Doe')
+        self.assertEqual(first_post_data['email'], 'john@example.com')
+        self.assertEqual(first_post_data['content'], "Hello world, I'm John!")
 
-#         # Assert the second post.
-#         second_post_data = data['timeline_posts'][1]
-#         self.assertIsInstance(second_post_data, dict)
-#         self.assertEqual(second_post_data['id'], second_post.id)
-#         self.assertEqual(second_post_data['name'], 'Jane Doe')
-#         self.assertEqual(second_post_data['email'], 'jane@example.com')
-#         self.assertEqual(second_post_data['content'], "Hello world, I'm Jane!")
+        # Assert the second post.
+        second_post_data = data['timeline_posts'][1]
+        self.assertIsInstance(second_post_data, dict)
+        self.assertEqual(second_post_data['id'], second_post.id)
+        self.assertEqual(second_post_data['name'], 'Jane Doe')
+        self.assertEqual(second_post_data['email'], 'jane@example.com')
+        self.assertEqual(second_post_data['content'], "Hello world, I'm Jane!")
